5adba83807287f67c22bb6c5c5c9ae111fad2330
Randall Leeds <randall@bleeds.info>
2013-11-05 14:19:11 -0800

    Updated events template

--- events.php
+++ events.php
@@ -3,32 +3,32 @@
 Template Name: Events
 */
 ?>
-<?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+
+<?php get_template_parts( array( 'parts/shared/html-header' , 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 <article>
-  <?php the_content(); ?>
-  
-  <?php if(get_field('hyp_event')): ?> 
-     <ul>
-     <?php while(has_sub_field('hyp_event')): ?>
-       <li>
-         <h3 class="noborder"><?php the_sub_field('hyp_event_title'); ?></h3>
-         <span class="eventdate"><?php the_sub_field('hyp_event_date'); ?></span>
-         <a href="<?php the_sub_field('hyp_event_link_url'); ?>" title="<?php the_sub_field('hyp_event_link_title'); ?>" class="strong">
-		   <?php the_sub_field('hyp_event_link_title'); ?>
-         </a>
-         <div class="em">
-           <?php the_sub_field('hyp_event_summary'); ?>
-         </div>
-       </li>
-     <?php endwhile; ?>
-     </ul>
-  <?php endif; ?>
-  
-  
+	<?php the_content(); ?>
+
+	<?php if(get_field('hyp_event')): ?>
+	<ul style="padding-left:0;" id="eventslist">
+		<?php while(has_sub_field('hyp_event')): ?>
+		<li class="pressunit">
+			<div class="text">
+				<h3 class="noborder"><?php the_sub_field('hyp_event_title'); ?></h3>
+				<a href="<?php the_sub_field('hyp_event_link_url'); ?>" title="<?php the_sub_field('hyp_event_link_title'); ?>" class="strong"><?php the_sub_field('hyp_event_link_title'); ?></a> &ndash; <span><?php $date = DateTime::createFromFormat('Ymd', get_sub_field('hyp_event_date'));
+					echo $date->format('F d, Y'); ?></span>
+				<div class="em">
+					<?php the_sub_field('hyp_event_summary'); ?>
+				</div>
+			</div>
+		</li>
+		<?php endwhile; ?>
+	</ul>
+	<?php endif; ?>
 </article>
+
 <div class="bigspacer"></div>
 <?php endwhile; ?>
 
-<?php get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
+<?php get_template_parts( array( 'parts/shared/footer' , 'parts/shared/html-footer' ) ); ?>
