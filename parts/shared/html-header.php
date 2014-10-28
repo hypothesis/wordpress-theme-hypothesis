fb52e56b5db06a60fde498f490b71c8505f2f259
RawKStar77 <RawKStar77@aol.com>
2014-10-27 17:44:08 -0700

    Add favicon to theme.
    We were having problems with the favicon not showing on certain pages. This is probably because the plugin we use to do this, "All in one favicon," is two years out of date. When this is merged and the site updated we should deactivate and delete that plugin.

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -10,7 +10,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- Remove if you're not building a responsive site. (But then why would you do such a thing?) -->
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico"/>
 		<?php wp_head(); ?>
 	</head>
 	<body id="pagetainer">
