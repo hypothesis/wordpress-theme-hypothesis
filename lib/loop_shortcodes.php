a9615add553a7a4fd123f4f75cf61e4099ef41d7
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-02-23 14:50:12 -0800

    Small adjustments to spacing on press page.

--- lib/loop_shortcodes.php
+++ lib/loop_shortcodes.php
@@ -25,7 +25,7 @@
               <div class="media-body">
                 <h3 class="noborder">'.get_post_meta( get_the_ID(), 'outlet', true ).'</h3>
                 <a class="strong" href="'.get_post_meta( get_the_ID(), 'link', true ).'">'.get_the_title().'</a>
-                <div class="caption hypo-small">
+                <div class="hypo-date hypo-small">
                   <span>'.get_post_meta( get_the_ID(), 'date', true ).'</span>
                 </div>
                 <div class="em">"'.get_post_meta( get_the_ID(), 'excerpt', true ).'</div>
