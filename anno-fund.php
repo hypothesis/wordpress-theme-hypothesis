9d66f941a5e71bded509a9db2471a031523a512e
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-03-05 17:41:47 -0800

    Add template for annotation fund page.

---
+++ anno-fund.php
@@ -0,0 +1,216 @@
+<?php
+/*
+Template Name: Anno-fund
+*/
+?>
+
+<div class="container">
+<!-- About Section -->
+    <section id="about" class="hypo-space-at-bottom">
+        <div class="row">
+            <div class="col-lg-12 jumbotron hypo-tron">
+                <h2>A $50k fund supporting Open Annotation</h2>
+                <p>The Open Annotation standard promises to become a new layer over human knowledge. $50k annually has been contributed by Hypothes.is and other donors, all as non-profit, charitable donations to make this a reality.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <h4 class="service-heading">The Need</h4>
+                <p>
+                We must take annotation to the boundaries of the web.
+                It must be integrated with countless applications,
+                technologies, libraries, products, web sites and more.
+                </p>
+                <p>
+                In many cases, the projects exist, and the developers are
+                eager to do the work, but schedules and the demands of
+                producing sustainable income prohibits focus on the effort.
+                What's needed is a relatively small amount of funding to
+                facilitate that focus.
+                </p>
+                <p>
+                Grants as small as $2-10k can open up entirely new use
+                cases and integrations with important platforms and
+                libraries.</p>
+            </div>
+            <div class="col-md-4">
+                <h4 class="service-heading">The Solution</h4>
+                <p>A grantmaking process that
+                would solicit and receive proposals to expand the existing
+                universe of open source software that implements or works
+                with the Open Annotation specification. Funds are
+                distributed as grants with only limited
+                obligations.
+                </p>
+                <p>
+                Applications do not need to relate to any particular
+                annotation platform or technology, but only to generally
+                integrate or provide annotation functionality and be open
+                source in nature.
+                </p>
+            </div>
+            <div class="col-md-4">
+                <h4 class="service-heading">The Grants</h4>
+                <p>
+                Awards are made on a rolling basis, after review of existing applications.
+                Grant amounts are based on a combination of the applicants’ assessment of
+                difficulty and the judges assessment of the utility of the proposed
+                integration, difficulty and novelty in illustrating new use cases for
+                annotation.
+                </p>
+                <p>
+                Small grants will be made directly to individuals or small
+                organizations in amounts ranging from <strong>$2k to $10k</strong>
+                depending on the work to be done and other considerations.
+                Funds will be paid 50% upon award and 50% upon completion.
+                </p>
+            </div>
+        </div>
+    </section>
+
+<!-- Projects Section -->
+    <section id="portfolio" class="hypo-space-at-bottom">
+        <div class="row hypo-bottom-padding">
+            <div class="col-lg-12">
+                <h2 class="section-heading">Projects</h2>
+                <h3 class="section-subheading text-muted">
+                  Completed Projects funded by the Open Annotation Fund.
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                  <h4><a href="https://hypothes.is/blog/fund-cross-browser-support/">Cross-browser Support</a></h4>
+                    <p class="">for Annotator</p>
+                    <hr>
+                    The first award of $7,500 went to the Open Gov Foundation for work by Chris Birk and Bill Hunt for cross-browser support in Annotator.</p>
+            </div>
+        </div>
+    </section>
+
+<!-- Timeline Section -->
+    <section id="timeline" class="hypo-space-at-bottom">
+            <div class="row hypo-bottom-padding">
+                <div class="col-lg-12">
+                    <h2 class="section-heading">Timeline</h2>
+                    <h3 class="section-subheading text-muted">a brief history of the Open Annotation Fund</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="timeline">
+                        <li>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>April 1, 2014</h4>
+                                    <h4 class="subheading">Fund launched</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="">The Open Annotation Fund launched with an announcement on the
+                                    <a href="https://hypothes.is/blog/an-open-annotation-fund/">Hypothes.is blog</a></p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>June 6, 2014</h4>
+                                    <h4 class="subheading">
+                                      <a href="https://hypothes.is/blog/fund-cross-browser-support/">
+                                        Grant: Cross-browser Support
+                                      </a>
+                                    </h4>
+                                </div>
+                                <div class="timeline-body">
+                                  <p>The first award of $7,500 went to the <a href="http://www.opengovfoundation.org/">Open Gov Foundation</a> for work by Chris Birk and Bill Hunt for cross-browser support in Annotator.</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    </section>
+
+
+<!-- Team Section -->
+    <section id="team" class="hypo-space-at-bottom">
+        <div class="row hypo-bottom-padding">
+            <div class="col-lg-12">
+                <h2 class="section-heading">The Judges</h2>
+                <h3 class="section-subheading">
+                  Our judges were selected from around the open source, knowledge, and annotation communities.
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <h4>Phil Desenne</h4>
+                    <p class="">Annotation Lead, HarvardX / edX</p>
+                </div>
+            </div>
+            
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <h4>Randall Leeds</h4>
+                    <p class="">Lead Developer, Hypothes.is</p>
+                </div>
+            </div>
+            
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <h4>Rob Sanderson</h4>
+                    <p class="">Co-Chair, Open Annotation Community Group, Stanford University</p>
+                </div>
+            </div>
+            
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <h4>Doug Schepers</h4>
+                    <p class="">Developer Relations, W3C</p>
+                </div>
+            </div>
+            
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <h4>Nick Stenning</h4>
+                    <p class="">Lead Developer, Annotator, Hypothes.is</p>
+                </div>
+            </div>        
+        </div>
+    </section>
+
+    <section id="contact" class="hypo-space-at-bottom">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="section-heading">Submit Your Application</h2>
+                <p>Applicants should submit a one page summary overview of their proposal to fund@hypothes.is. The summary should cover:</p>
+                <ul class="list">
+                  <li>The overall goal of your larger work</li>
+                  <li>How annotation figures in your proposal</li>
+                  <li>The team members that would do the work</li>
+                  <li>The relevant product, service or code library that any work would affect.</li>
+                  <li>The end product code, feature or product that would be produced as result of the grant.</li>
+                  <li>Where the final blog post summarizing the work will be published.</li>
+                  <li>A rough sizing in terms of development time required to complete the effort.</li>
+                </ul>
+                <p>Applicants Agree that:</p>
+                <ul class="list hypo-bottom-padding">
+                  <li>All design elements and software produced as a result of their work would
+                    be made open source on a maximally permissive license (MIT, BSD, Apache, etc)
+                    without restrictions on subsequent use (including commercial).</li>
+                  <li>The software will be made publicly available either directly or via a
+                    popular open-source software repository.</li>
+                  <li>The software will be well documented.</li>
+                  <li>They will blog about the work they’ve done, describing the purpose in
+                    doing it, the particulars of the effort, the result and the future
+                    opportunities.</li>
+                  <li>They will agree to provide basic support to the developer community for a
+                    year after the award.</li>
+                </ul>
+                <p><a href="https://docs.google.com/a/hypothes.is/forms/d/1FOIB5ZkmxrL73fWpdeeoNrDPqruTLXeOLJW175jiG1I/viewform" class="btn btn-primary btn-lg">Apply</a></p>
+            </div>
+        </div>
+    </section>
+</div>
