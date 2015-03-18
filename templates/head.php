468f094794e189bdecc4b5ef9bf4f43b8da93330
Randall Leeds <randall@bleeds.info>
2015-03-18 06:04:35 -0700

    Revert "Add title tag"
    This reverts commit 60f672071f1497489dbc9bc1f513bb0bf261fed9.
    
    Looks like our theme base has this already patched into wp_head.

--- templates/head.php
+++ templates/head.php
@@ -4,7 +4,6 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php wp_title(''); ?></title>
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
