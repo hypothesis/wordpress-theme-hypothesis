0c55f18e5d3283938ba9d9f805f1c8abe61d9178
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-03-19 13:42:36 -0700

    Add full width template for use with team page.

---
+++ fullwidth-page.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Template Name: Full Width Page
+*/
+?>
+
+<div class="container hypo-space-at-bottom">
+	<?php while (have_posts()) : the_post(); ?>
+	  <?php get_template_part('templates/page', 'header'); ?>
+	  <?php get_template_part('templates/content', 'page'); ?>
+	<?php endwhile; ?>
+</div>
