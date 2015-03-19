b4022becac2d095a3e7560421c8014d9b143d286
Randall Leeds <randall@bleeds.info>
2015-03-18 22:25:12 -0700

    Clean up a bunch of styles and markup
    Get rid of:
    
    - .hypo-caption
    - .hypo-screenshot
    - .hypo-tron
    - .hypo-video
    
    A bunch of other nonsense.

--- templates/loop_shortcodes.php
+++ templates/loop_shortcodes.php
@@ -25,7 +25,7 @@
               <div class="media-body">
                 <h3 class="noborder">'.get_post_meta( get_the_ID(), 'outlet', true ).'</h3>
                 <a class="strong" href="'.get_post_meta( get_the_ID(), 'link', true ).'">'.get_the_title().'</a>
-                <p class="hypo-small">
+                <p class="small">
                   <span>'.get_post_meta( get_the_ID(), 'date', true ).'</span>
                 </p>
                 <p>'.get_post_meta( get_the_ID(), 'excerpt', true ).'</p>
