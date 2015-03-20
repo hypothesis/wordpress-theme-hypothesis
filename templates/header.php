b1e52162d120fbe437afff83e5afdb76e09465d1
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-03-20 14:53:10 -0700

    Update logo.

--- templates/header.php
+++ templates/header.php
@@ -9,7 +9,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">Hypothes<span class="red">.</span>is</a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
