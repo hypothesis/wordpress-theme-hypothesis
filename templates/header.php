67e9b6d47b38a74571390ec4c93bcd00e71c31eb
Randall Leeds <randall@bleeds.info>
2015-05-05 16:04:31 -0700

    Clean up some CSS
    Rework the brand header, remove changes which can be disabled by
    configuration rather than code (comments), and fix up the social
    media icons.

--- templates/header.php
+++ templates/header.php
@@ -1,36 +1,32 @@
-<header class="banner" role="banner">
-  <!-- Temporary hiring banner -->
-  <div style="padding-top: 10px; padding-bottom: 10px; min-height: 10px; margin-bottom: -20px; color: rgb(255, 255, 255); background-color: rgb(189, 28, 43);" class="navbar navbar-default navbar-static-top">
-    <div class="container">
-      <span>We're hiring <a href="https://hypothes.is/jobs/#developer" style="color: rgb(255, 255, 255); text-decoration: underline;">developers</a> and <a href="https://hypothes.is/jobs/#designer" style="color: rgb(255, 255, 255); text-decoration: underline;">designers</a>.</span>
-    </div>
+<!-- Temporary hiring banner -->
+<header class="banner hiring" role="banner">
+  <div class="container">
+    <span>We're hiring <a href="https://hypothes.is/jobs/#developer">developers</a> and <a href="https://hypothes.is/jobs/#designer">designers</a>.</span>
   </div>
-  <!-- / Temporary hiring banner -->
+</header>
+<!-- / Temporary hiring banner -->
 
-  <div class="navbar navbar-default navbar-static-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">Hypothes<span class="red">.</span>is</a>
-      </div>
-
-      <nav class="collapse navbar-collapse" role="navigation">
+<header class="navbar navbar-default navbar-static-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">Hypothes<span class="red">.</span>is</a>
+    </div>
+    <nav class="collapse navbar-collapse" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-        endif;
-        ?>
-
-        <ul class="nav navbar-nav pull-right login">
-          <li><a href="https://hypothes.is/login">Login</a></li>
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
+      endif;
+      ?>
+      <ul class="nav navbar-nav pull-right login">
+        <li><a href="https://hypothes.is/login">Login</a></li>
           <li><a href="https://hypothes.is/register">Sign up</a></li>
-        </ul>
-      </nav>
-    </div>
+      </ul>
+    </nav>
   </div>
 </header>
