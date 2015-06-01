e5570aed2aa027792e4bd3cc94cb7199a39d843a
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-06-01 14:50:04 -0700

    Kill WP comments.

--- templates/content-single.php
+++ templates/content-single.php
@@ -10,6 +10,5 @@
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
   </article>
 <?php endwhile; ?>
