44a4c9572c8f88f425d2df42322b247d8532e067
Robert Knight <robertknight@gmail.com>
2015-12-12 12:01:59 +0000

    Remove font-awesome from the blog's styles
    This was previously used for social media icons in the footer
    of the blog and has been replaced by the icomoon font from
    the main repository in assets/vendor/hypothesis-shared

--- templates/head.php
+++ templates/head.php
@@ -4,6 +4,5 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <?php wp_head(); ?>
   </head>
