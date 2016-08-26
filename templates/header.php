a39b97410300360c9c2f7e927e1c8836bece30e2
Lena <lenazun@gmail.com>
2016-08-26 08:40:23 -0700

    Changes to hiring banner
    Changes hiring banner to generic message

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <!-- Temporary hiring banner -->
 <header class="banner hiring" role="banner">
   <div class="container">
-    <span>We're hiring a <a href="https://hypothes.is/jobs/#product-manager">Product Manager</a>.</span>
+    <span>We're <a href="https://hypothes.is/jobs/">hiring!</a>.</span>
   </div>
 </header>
 <!-- / Temporary hiring banner -->
