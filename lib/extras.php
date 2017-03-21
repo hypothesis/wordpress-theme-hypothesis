cade433d66f1c0df5cb128a1a9bdf27a9b90e676
Nate Angell <nate@xolotl.org>
2017-03-21 00:11:52 -0700

    enable current WP site to display read more link to full post on manual excerpts

--- lib/extras.php
+++ lib/extras.php
@@ -27,7 +27,18 @@ add_filter('body_class', __NAMESPACE__ . '\\body_class');
 /**
  * Clean up the_excerpt()
  */
+
+// Display read more link when there is an automatically entered excerpt
 function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+// Display read more link when there is a manually entered excerpt
+function excerpt_read_more($excerpt) {
+  if (has_excerpt() && !is_attachment()) {
+    $excerpt .= ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
+  }
+    return $excerpt;
+}
+add_filter('get_the_excerpt', __NAMESPACE__ . '\\excerpt_read_more');
