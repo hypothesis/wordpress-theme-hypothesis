05553a12886469a023e81729e5548551a32def64
Nate Angell <nate@xolotl.org>
2017-03-23 09:19:06 -0700

    cleaning up spacing issue and improving comments

--- lib/extras.php
+++ lib/extras.php
@@ -28,17 +28,17 @@ add_filter('body_class', __NAMESPACE__ . '\\body_class');
  * Clean up the_excerpt()
  */
 
-// Display read more link when there is an automatically entered excerpt
+// Display read more link when the excerpt is generated automatically
 function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
 
-// Display read more link when there is a manually entered excerpt
+// Display read more link when the excerpt is entered manually
 function excerpt_read_more($excerpt) {
   if (has_excerpt() && !is_attachment()) {
     $excerpt .= ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
   }
-    return $excerpt;
+  return $excerpt;
 }
 add_filter('get_the_excerpt', __NAMESPACE__ . '\\excerpt_read_more');
